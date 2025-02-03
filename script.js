d5a021d4fdb5de1f1ba1fb49463df972a800ba85
luo0715 <a0975140151j@gmail>
2025-02-03 20:24:05 +0800

    2025/02/03

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ const form1 = document.querySelectorAll(".form--1");
 const form2 = document.querySelectorAll(".form--2");
 const form3 = document.querySelectorAll(".form--3");
 const form4 = document.querySelectorAll(".form--4");
+const btnNext1 = document.querySelector(".btn--next--1");
+const btnNext2 = document.querySelector(".btn--next--2");
+const btnNext3 = document.querySelector(".btn--next--3");
+const btnNext4 = document.querySelector(".btn--next--4");
 //////////////////////////////screen loading///////////////////////
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -172,212 +176,366 @@ const init = function () {
 };
 init();
 
+//////////////////////////////initial nextBtn///////////////////////
+
+formBtn.forEach((btn) => (btn.disabled = true));
+
 //////////////////////////////form validation///////////////////////
 
-const formSubmitCheck1 = function (status) {
-  formBtn.forEach((btn) => {
-    btn.addEventListener("click", function (e) {
-      e.preventDefault();
-      
-      const name = document.querySelector(".input--1");
-      const phone = document.querySelector(".input--2");
-      const birthYear = document.querySelector(".input--4");
-      const people = document.querySelector(".input--6");
-      const mail = document.querySelector(".input--7");
-      const formComplete = [0, 0, 0, 0, 0];
-
-      const containsNumber = function (name) {
-        for (let i = 0; i < name.length; i++) {
-          if (!isNaN(name[i]) && name[i] !== " ") {
-            return true;
-          }
-        }
-        return false;
-      };
-
-     
-        if (name.value === "" || containsNumber(name.value)) {
-          alert("姓名不可為空或者包含數字");
-          formComplete[0] = 1;
-        } else {
-          formComplete[0] = 0;
-        }
-
-        if (phone.value === "") {
-          alert("手機號碼不可為空");
-          formComplete[1] = 1;
-        } else if (isNaN(phone.value)) {
-          alert("請輸入正確手機格式");
-          formComplete[1] = 1;
-        } else if (phone.value.length !== 10 || !phone.value.startsWith("09")) {
-          alert("請輸入正確手機格式");
-          formComplete[1] = 1;
-        } else {
-          formComplete[1] = 0;
-        }
-
-        let currentYear = new Date().getFullYear();
-        if (birthYear.value === "") {
-          alert("出生年份不可為空");
-          formComplete[2] = 1;
-        } else if (isNaN(birthYear.value)) {
-          alert("請輸入正確出生年份格式");
-          formComplete[2] = 1;
-        } else if (birthYear.value < 1911 || birthYear.value > currentYear) {
-          alert("請輸入正確出生年份格式");
-          formComplete[2] = 1;
-        } else {
-          formComplete[2] = 0;
-        }
-
-        if (people.value === "") {
-          alert("人數不可為空");
-          formComplete[3] = 1;
-        } else if (people.value <= 0) {
-          alert("人數至少為1人");
-          formComplete[3] = 1;
-        } else {
-          formComplete[3] = 0;
-        }
-
-        if (
-          mail.value !== "" &&
-          (!mail.value.includes("@") || !mail.value.includes("."))
-        ) {
-          alert("請輸入正確電子郵件格式");
-          formComplete[4] = 1;
-        } else if (mail.value === "") {
-          alert("電子郵件不可為空");
-          formComplete[4] = 1;
-        } else {
-          formComplete[4] = 0;
-        }
-
-        if (formComplete.every((key) => key === 0)) {
-          console.log(123);
-        }
-      
-    });
-  });
+const containsNumber = function (name) {
+  for (let i = 0; i < name.length; i++) {
+    if (!isNaN(name[i]) && name[i] !== " ") {
+      return true;
+    }
+  }
+  return false;
 };
 
-formSubmitCheck1();
+inputs.forEach((input) => {
+  input.addEventListener("input", function () {
+    const name = document.querySelector(".input--1");
+    const phone = document.querySelector(".input--2");
+    const birthYear = document.querySelector(".input--4");
+    const people = document.querySelector(".input--6");
+    const mail = document.querySelector(".input--7");
+    const formComplete = [0, 0, 0, 0, 0];
 
-const formSubmitCheck2 = function () {
-  formBtn.forEach((btn) => {
-    btn.addEventListener("click", function (e) {
-      e.preventDefault();
+    if (name.value === " " || containsNumber(name.value)) {
+      name.style.backgroundColor = "#bb000073";
+      formComplete[0] = 1;
+    } else {
+      formComplete[0] = 0;
+      name.style.backgroundColor = "transparent";
+    }
 
-      const name = document.querySelector(".input--8");
-      const phone = document.querySelector(".input--9");
-      const plan = document.querySelector(".input--11");
-      const people = document.querySelector(".input--12");
-      const mail = document.querySelector(".input--13");
-      const formComplete = [0, 0, 0, 0, 0];
+    if (phone.value === "") {
+      // alert("手機號碼不可為空");
+      phone.style.backgroundColor = "#bb000073";
+      formComplete[1] = 1;
+    } else if (isNaN(phone.value)) {
+      // alert("請輸入正確手機格式");
+      formComplete[1] = 1;
+      phone.style.backgroundColor = "#bb000073";
+    } else if (phone.value.length !== 10 || !phone.value.startsWith("09")) {
+      // alert("請輸入正確手機格式");
+      formComplete[1] = 1;
+      phone.style.backgroundColor = "#bb000073";
+    } else {
+      formComplete[1] = 0;
+      phone.style.backgroundColor = "transparent";
+    }
 
-      const containsNumber = function (name) {
-        for (let i = 0; i < name.length; i++) {
-          if (!isNaN(name[i]) && name[i] !== " ") {
-            return true;
-          }
-        }
-        return false;
-      };
+    let currentYear = new Date().getFullYear();
+    if (birthYear.value === "") {
+      // alert("出生年份不可為空");
+      birthYear.style.backgroundColor = "#bb000073";
+      formComplete[2] = 1;
+    } else if (isNaN(birthYear.value)) {
+      // alert("請輸入正確出生年份格式");
+      birthYear.style.backgroundColor = "#bb000073";
+      formComplete[2] = 1;
+    } else if (birthYear.value < 1911 || birthYear.value > currentYear) {
+      // alert("請輸入正確出生年份格式");
+      birthYear.style.backgroundColor = "#bb000073";
+      formComplete[2] = 1;
+    } else {
+      formComplete[2] = 0;
+      birthYear.style.backgroundColor = "transparent";
+    }
 
-      if (name.value === "" || containsNumber(name.value)) {
-        alert("姓名不可為空或者包含數字");
-        formComplete[0] = 1;
-      } else {
-        formComplete[0] = 0;
-      }
+    if (people.value === "") {
+      // alert("人數不可為空");
+      people.style.backgroundColor = "#bb000073";
+      formComplete[3] = 1;
+    } else if (people.value <= 0) {
+      // alert("人數至少為1人");
+      people.style.backgroundColor = "#bb000073";
+      formComplete[3] = 1;
+    } else {
+      formComplete[3] = 0;
+      people.style.backgroundColor = "transparent";
+    }
 
-      if (phone.value === "") {
-        alert("手機號碼不可為空");
-        formComplete[1] = 1;
-      } else if (isNaN(phone.value)) {
-        alert("請輸入正確手機格式");
-        formComplete[1] = 1;
-      } else if (phone.value.length !== 10 || !phone.value.startsWith("09")) {
-        alert("請輸入正確手機格式");
-        formComplete[1] = 1;
-      } else {
-        formComplete[1] = 0;
-      }
+    if (
+      mail.value !== "" &&
+      (!mail.value.includes("@") || !mail.value.endsWith(".com"))
+    ) {
+      // alert("請輸入正確電子郵件格式");
+      mail.style.backgroundColor = "#bb000073";
+      formComplete[4] = 1;
+    } else if (mail.value === "") {
+      // alert("電子郵件不可為空");
+      mail.style.backgroundColor = "#bb000073";
+      formComplete[4] = 1;
+    } else {
+      formComplete[4] = 0;
+      mail.style.backgroundColor = "transparent";
+    }
 
-      if (plan.value === "") {
-        alert("公司名稱不可為空");
-        formComplete[2] = 1;
-      } else {
-        formComplete[2] = 0;
-      }
-
-      if (people.value === "") {
-        alert("人數不可為空");
-        formComplete[3] = 1;
-      } else if (people.value <= 0) {
-        alert("人數至少為1人");
-        formComplete[3] = 1;
-      } else {
-        formComplete[3] = 0;
-      }
-
-      if (
-        mail.value !== "" &&
-        (!mail.value.includes("@") || !mail.value.includes("."))
-      ) {
-        alert("請輸入正確電子郵件格式");
-        formComplete[4] = 1;
-      } else if (mail.value === "") {
-        alert("電子郵件不可為空");
-        formComplete[4] = 1;
-      } else {
-        formComplete[4] = 0;
-      }
-
-      if (formComplete.every((key) => key === 0)) {
-        console.log(123);
-      }
-    });
+    if (formComplete.every((key) => key === 0)) {
+      console.log("form1 驗證通過");
+      btnNext1.disabled = false;
+    } else {
+      console.log("驗證失敗");
+      btnNext1.disabled = true;
+    }
   });
-};
+});
 
-formSubmitCheck2();
+// const handleFormSubmit2 = function (e) {
+//   e.preventDefault();
+
+//   if ([...form2].some((form) => form.classList.contains("form--hidden")))
+//     return;
+
+//   const name = document.querySelector(".input--8");
+//   const phone = document.querySelector(".input--9");
+//   const plan = document.querySelector(".input--11");
+//   const people = document.querySelector(".input--12");
+//   const mail = document.querySelector(".input--13");
+//   const formComplete = [0, 0, 0, 0, 0];
+
+//   if (name.value === "" || containsNumber(name.value)) {
+//     alert("姓名不可為空或者包含數字");
+//     formComplete[0] = 1;
+//   } else {
+//     formComplete[0] = 0;
+//   }
+
+//   if (phone.value === "") {
+//     alert("手機號碼不可為空");
+//     formComplete[1] = 1;
+//   } else if (isNaN(phone.value)) {
+//     alert("請輸入正確手機格式");
+//     formComplete[1] = 1;
+//   } else if (phone.value.length !== 10 || !phone.value.startsWith("09")) {
+//     alert("請輸入正確手機格式");
+//     formComplete[1] = 1;
+//   } else {
+//     formComplete[1] = 0;
+//   }
+
+//   if (plan.value === "") {
+//     alert("公司名稱不可為空");
+//     formComplete[2] = 1;
+//   } else {
+//     formComplete[2] = 0;
+//   }
+
+//   if (people.value === "") {
+//     alert("人數不可為空");
+//     formComplete[3] = 1;
+//   } else if (people.value <= 0) {
+//     alert("人數至少為1人");
+//     formComplete[3] = 1;
+//   } else {
+//     formComplete[3] = 0;
+//   }
+
+//   if (
+//     mail.value !== "" &&
+//     (!mail.value.includes("@") || !mail.value.includes("."))
+//   ) {
+//     alert("請輸入正確電子郵件格式");
+//     formComplete[4] = 1;
+//   } else if (mail.value === "") {
+//     alert("電子郵件不可為空");
+//     formComplete[4] = 1;
+//   } else {
+//     formComplete[4] = 0;
+//   }
+
+//   if (formComplete.every((key) => key === 0)) {
+//     console.log("form2 驗證通過");
+// 進行下一步動作
+//   }
+// };
+
+btnNext1.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("you can start next step");
+});
+// btnNext2.addEventListener("click", handleFormSubmit2);
+
+// const formSubmitCheck1 = function (status) {
+//   btnNext1.addEventListener("click", function (e) {
+//     e.preventDefault();
+
+//     const name = document.querySelector(".input--1");
+//     const phone = document.querySelector(".input--2");
+//     const birthYear = document.querySelector(".input--4");
+//     const people = document.querySelector(".input--6");
+//     const mail = document.querySelector(".input--7");
+//     const formComplete = [0, 0, 0, 0, 0];
+
+//     if (status === true) {
+//       if (name.value === "" || containsNumber(name.value)) {
+//         alert("姓名不可為空或者包含數字");
+//         formComplete[0] = 1;
+//       } else {
+//         formComplete[0] = 0;
+//       }
+
+//       if (phone.value === "") {
+//         alert("手機號碼不可為空");
+//         formComplete[1] = 1;
+//       } else if (isNaN(phone.value)) {
+//         alert("請輸入正確手機格式");
+//         formComplete[1] = 1;
+//       } else if (phone.value.length !== 10 || !phone.value.startsWith("09")) {
+//         alert("請輸入正確手機格式");
+//         formComplete[1] = 1;
+//       } else {
+//         formComplete[1] = 0;
+//       }
+
+//       let currentYear = new Date().getFullYear();
+//       if (birthYear.value === "") {
+//         alert("出生年份不可為空");
+//         formComplete[2] = 1;
+//       } else if (isNaN(birthYear.value)) {
+//         alert("請輸入正確出生年份格式");
+//         formComplete[2] = 1;
+//       } else if (birthYear.value < 1911 || birthYear.value > currentYear) {
+//         alert("請輸入正確出生年份格式");
+//         formComplete[2] = 1;
+//       } else {
+//         formComplete[2] = 0;
+//       }
+
+//       if (people.value === "") {
+//         alert("人數不可為空");
+//         formComplete[3] = 1;
+//       } else if (people.value <= 0) {
+//         alert("人數至少為1人");
+//         formComplete[3] = 1;
+//       } else {
+//         formComplete[3] = 0;
+//       }
+
+//       if (
+//         mail.value !== "" &&
+//         (!mail.value.includes("@") || !mail.value.includes("."))
+//       ) {
+//         alert("請輸入正確電子郵件格式");
+//         formComplete[4] = 1;
+//       } else if (mail.value === "") {
+//         alert("電子郵件不可為空");
+//         formComplete[4] = 1;
+//       } else {
+//         formComplete[4] = 0;
+//       }
+
+//       if (formComplete.every((key) => key === 0)) {
+//         console.log(123);
+//       }
+//     }
+//   });
+// };
+// formSubmitCheck1(active);
+
+// const formSubmitCheck2 = function (status) {
+//   btnNext2.addEventListener("click", function (e) {
+//     e.preventDefault();
+
+//     const name = document.querySelector(".input--8");
+//     const phone = document.querySelector(".input--9");
+//     const plan = document.querySelector(".input--11");
+//     const people = document.querySelector(".input--12");
+//     const mail = document.querySelector(".input--13");
+//     const formComplete = [0, 0, 0, 0, 0];
+
+//     if (!status) return;
+//     if (name.value === "" || containsNumber(name.value)) {
+//       alert("姓名不可為空或者包含數字");
+//       formComplete[0] = 1;
+//     } else {
+//       formComplete[0] = 0;
+//     }
+
+//     if (phone.value === "") {
+//       alert("手機號碼不可為空");
+//       formComplete[1] = 1;
+//     } else if (isNaN(phone.value)) {
+//       alert("請輸入正確手機格式");
+//       formComplete[1] = 1;
+//     } else if (phone.value.length !== 10 || !phone.value.startsWith("09")) {
+//       alert("請輸入正確手機格式");
+//       formComplete[1] = 1;
+//     } else {
+//       formComplete[1] = 0;
+//     }
+
+//     if (plan.value === "") {
+//       alert("公司名稱不可為空");
+//       formComplete[2] = 1;
+//     } else {
+//       formComplete[2] = 0;
+//     }
+
+//     if (people.value === "") {
+//       alert("人數不可為空");
+//       formComplete[3] = 1;
+//     } else if (people.value <= 0) {
+//       alert("人數至少為1人");
+//       formComplete[3] = 1;
+//     } else {
+//       formComplete[3] = 0;
+//     }
+
+//     if (
+//       mail.value !== "" &&
+//       (!mail.value.includes("@") || !mail.value.includes("."))
+//     ) {
+//       alert("請輸入正確電子郵件格式");
+//       formComplete[4] = 1;
+//     } else if (mail.value === "") {
+//       alert("電子郵件不可為空");
+//       formComplete[4] = 1;
+//     } else {
+//       formComplete[4] = 0;
+//     }
+
+//     if (formComplete.every((key) => key === 0)) {
+//       console.log(123);
+//     }
+//   });
+// };
 
 //////////////////////////////formChange///////////////////////
 
 const components = document.querySelectorAll(".component");
 
-const formChange = function () {
-  components.forEach((component) => {
-    component.addEventListener("click", function (e) {
-      e.preventDefault();
-      const btn = e.target.closest(".component");
-      const formNum = +e.target.dataset.num;
+components.forEach((component) => {
+  component.addEventListener("click", function (e) {
+    e.preventDefault();
+    const btn = e.target.closest(".component");
+    const formNum = +e.target.dataset.num;
 
-      inputs.forEach((form) => {
-        form.classList.add("form--hidden");
-      });
-
-      formBtn.forEach((btn) => btn.classList.add("form--hidden"));
-
-      document.querySelectorAll(`.form--${formNum}`).forEach((form) => {
-        form.classList.remove("form--hidden");
-
-        inputs.forEach((input) => {
-          if (!input.classList.contains("option")) {
-            input.value = "";
-          }
-        });
-      });
-
-      components.forEach((comp) => {
-        comp.classList.remove("active");
-      });
-
-      btn.classList.add("active");
-
-      formSubmitCheck1(disable);
+    inputs.forEach((form) => {
+      form.classList.add("form--hidden");
     });
+
+    formBtn.forEach((btn) => btn.classList.add("form--hidden"));
+
+    document.querySelectorAll(`.form--${formNum}`).forEach((form) => {
+      form.classList.remove("form--hidden");
+
+      inputs.forEach((input) => {
+        if (!input.classList.contains("option")) {
+          input.value = "";
+        }
+      });
+    });
+
+    formBtn.forEach((btn) => {
+      btn.disabled = true;
+    });
+
+    components.forEach((comp) => {
+      comp.classList.remove("active");
+    });
+
+    btn.classList.add("active");
   });
-};
-formChange();
+});
